c23dec5ac4e35323f50400e5eb30ecbf217c9e67
royhairul <royhairulanam19@gmail.com>
2025-08-29 15:32:54 +0700

    feat: add component for navbar

---
+++ src/components/app-navbar.tsx
@@ -0,0 +1,99 @@
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarContent,
+  NavbarItem,
+  Link,
+  Button,
+} from "@heroui/react";
+
+import Image from "next/image";
+
+export default function AppNavbar() {
+  const menuItems = [
+    "Profile",
+    "Dashboard",
+    "Activity",
+    "Analytics",
+    "System",
+    "Deployments",
+    "My Settings",
+    "Team Settings",
+    "Help & Feedback",
+    "Log Out",
+  ];
+
+  return (
+    <Navbar disableAnimation isBordered>
+      {/* Left content (menu toggle on mobile + brand) */}
+      <NavbarContent justify="start" className="gap-3">
+        <NavbarMenuToggle className="sm:hidden" />
+        <NavbarBrand className="flex items-center gap-2">
+          <Image
+            src="/assets/logo.png"
+            alt="Promoin Logo"
+            width={120}
+            height={40}
+          />
+          {/* <p className="font-bold text-inherit hidden sm:block">Promoin</p> */}
+        </NavbarBrand>
+      </NavbarContent>
+
+      {/* Center content (desktop menu) */}
+      <NavbarContent className="hidden sm:flex gap-6" justify="center">
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Features
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link aria-current="page" color="warning" href="#">
+            Customers
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            Integrations
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+
+      {/* Right content (auth buttons) */}
+      <NavbarContent justify="end">
+        <NavbarItem className="hidden lg:flex">
+          <Link href="#">Login</Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Button as={Link} color="warning" href="#" variant="flat">
+            Sign Up
+          </Button>
+        </NavbarItem>
+      </NavbarContent>
+
+      {/* Mobile menu */}
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              className="w-full"
+              color={
+                index === 2
+                  ? "warning"
+                  : index === menuItems.length - 1
+                  ? "danger"
+                  : "foreground"
+              }
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
+  );
+}
